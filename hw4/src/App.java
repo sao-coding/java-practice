399e75bcb15a0492f27e0d84a060e3daaae44492
sao-coding <58339015+sao-coding@users.noreply.github.com>
2022-08-31 13:35:58 +0000

    hw4

--- hw4/src/App.java
+++ hw4/src/App.java
@@ -1,16 +1,25 @@
 public class App {
     public static void main(String[] args) {    
-        System.out.println(pow(2, 10));
-    
+        for (int i = -10; i <= 10; i++) {
+            System.out.printf("%d ^ %d = %f\n", 2, i, pow(2, i));
+        }
+        // System.out.println(5/2);
     }
 
-    public static double pow(double base, int exponent) {
-    
-        if (exponent == 0) return 1;
-        double y = pow(base, exponent / 2);
+    public static double pow(double x, int n) {
+        if (n == 0) return 1;
+        int m = n >= 0 ? n : -n; // 解決 n 為負數的問題
+        double y = pow(x, m / 2);
         y = y * y;
-        if (exponent % 2 == 1) y *= base;
-        return y;
-    
+
+        if (m % 2 == 1)
+            y = x * y;
+        return n > 0 ? y : 1 / y;
+        // if (n % 2 == 1) {
+        //     y = y * x;
+        // }
+        // return y;
+
+        // return n % 2 == 1 ? x * y : y;
     }
 }
